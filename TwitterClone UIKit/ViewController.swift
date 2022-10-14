31825a2dae398bfebd84978edb75e3b4746d5599
Mikhail Kolkov <kolkov.michael2014@yandex.ru>
2022-10-14 15:24:22 -0400

    Initial Commit

---
+++ TwitterClone UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone UIKit
+//
+//  Created by Mikhail Kolkov on 10/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
